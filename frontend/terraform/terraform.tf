d6fca686d6ce2fda69a85b55af701e74ad17e206
Tuangtnp <thanaphat.tuang@gmail.com>
2025-06-17 18:52:51 +0700

    Add Terraform version requirements and update information on the website.

--- frontend/terraform/terraform.tf
+++ frontend/terraform/terraform.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "resume-website-aws-tfstate-prod"
     key    = "terraform/frontend-state"
